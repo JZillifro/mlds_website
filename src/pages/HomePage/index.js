fea2f2afa36a4353a3fc4eebdca91b596be2a501
JZillifro <ZillifroJacob@gmail.com>
2020-02-08 20:53:25 -0600

    text adjustments to homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -11,15 +11,15 @@ class EventsPage extends Component {
         </header>
 
         <p>
-          We strive to encourage students to pursue and share their interests in the field of data science and machine learning. We hold weekly meetings on Monday nights in the form of technical and career related data science workshops, technical talks by faculty members, and presentations by guests from industry. We also plan extra social events where members will have the opportunity to meet with researchers and recruiters in the field. Beyond lectures, we support participation in online ML competitions, and generally pursue projects that interest us. Read more about last semester’s Kaggle competition and featured participants here.
+          We strive to encourage students to pursue and share their interests in the field of data science and machine learning. We hold weekly meetings on Monday nights in the form of technical and career related data science workshops, technical talks by faculty members, and presentations by guests from industry. We also plan extra social events where members will have the opportunity to meet with researchers and recruiters in the field. Beyond lectures, we support participation in online ML competitions, and generally pursue projects that interest us.
         </p>
 
         <p>
-          Membership is free and open to all UT students – both undergraduate and graduate. If you are new to machine learning and data science, we’d love to have you join us! Come to meetings on Monday nights @ 6:00 in EER 3.646 and check out our resources page for tips on how to get started. You can also sign up for our weekly newsletter below.
+          Membership is free and open to all UT students – both undergraduate and graduate. If you are new to machine learning and data science, we’d love to have you join us! Come to meetings on Tuesday nights @ 6:30 in EER 3.646 and check out our resources page for tips on how to get started.
         </p>
 
         <p>
-          Access our Facebook group to learn more about meeting times/locations, topics covered, and competitions.
+          Access our Facebook group or the events page to learn more about meeting times/locations, topics covered, and competitions.
         </p>
       </div>
     );
